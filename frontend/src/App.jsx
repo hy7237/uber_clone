06eb5ead6c457a3569d7c23c886f7e83c79963ce
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-06-17 17:53:07 +0530

    frontend first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App=()=>
+{
+    return(      //javscript has a rule called Automatic Semicolan insertion..so when there is no statement in the same line of return than it injects semicolan and return nothing..so always return the anything from the same line of return like here(return()
+    
+        <div>hii himanshu</div>
+    );
+}
+
+export default App;
+
+
+
