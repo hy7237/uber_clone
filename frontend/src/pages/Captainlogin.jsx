1a30f7980b70558472c8f21a33524fbfa8af8a06
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-06-20 17:45:22 +0530

    userlogin and user-signup

--- frontend/src/pages/Captainlogin.jsx
+++ frontend/src/pages/Captainlogin.jsx
@@ -1,17 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Home=()=>
 {
+ const [email,setEmail]=useState('');{/*this is used to store the value of email input field and setEmail is used to update the value of email input field*/}
+ const [password,setPassword]=useState('');{/*this is used to store the value of password input field and setPassword is used to update the value of password input field*/}
+ const [captainData,setcaptainData]=useState({});{/*this is used to store the value of user data and setUserData is used to update the value of user data*/}
+ const submitHandler=(e)=>
+ {
+    e.preventDefault();{/*this is used to prevent the default behavior of the form which is to reload the page*/}
+    setcaptainData({
+        email: email,
+        password: password});{/*this is used to update the value of user data with the value of email and password input fields*/}
+        setEmail('');{/*this is used to clear the value of email input field*/}
+        setPassword('');{/*this is used to clear the value of password input field*/}
+ }
     return(
-        <div>
-            <div className='h-screen w-full'>
-                <div>
-                    <h2>Get started with Uber</h2>
-                    <button>Continue</button>
-                </div>
+        <div className='p-7 h-screen flex flex-col justify-between'>
+      <div>
+        <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
 
-            </div>
-        </div>
+        <form onSubmit={(e) => {
+          submitHandler(e)
+        }}>
+          <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+          <input
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="email"
+            placeholder='email@example.com'
+          />
+
+          <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+
+          <input
+            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            required type="password"
+            placeholder='password'
+          />
+
+          <button
+            className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+          >Login</button>
+
+        </form>
+        <p className='text-center'>Join a fleet? <Link to='/captain-signup' className='text-blue-600'>Register as a Captain</Link></p>
+      </div>
+      <div>
+        <Link
+          to='/login'
+          className='bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+        >Sign in as User</Link>
+      </div>
+    </div>
     )
 }
 
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,18 +1,100 @@
 import React from 'react'
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
 
-const Home=()=>
+
+const UserSignup=()=>
 {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [firstName,setFirstName]=useState('');
+    const [lastName,setLastName]=useState('');
+    const [userData,setUserData]=useState({});
+    const submitHandler=(e)=>
+    {
+        e.preventDefault();
+        setUserData({
+            username:
+            {firstName: firstName,
+            lastName: lastName,
+            },
+            email: email,
+            password: password
+        });
+        
+    setEmail('');{/*this is used to clear the value of email input field*/}
+    setFirstName('');{/*this is used to clear the value of first name input field*/}
+    setLastName('');{/*this is used to clear the value of last name input field*/}
+    setPassword('');{/*this is used to clear the value of password input field*/}
+    };
+
+    
     return(
         <div>
-            <div className='h-screen w-full'>
+            <div className='p-7 h-screen flex flex-col justify-between'>
                 <div>
-                    <h2>Get started with Uber</h2>
-                    <button>Continue</button>
-                </div>
+                    <img className='w-16 mb-10' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+                    <form onSubmit={(e)=>{
+                         
+                       submitHandler(e)
+                    }}>
+                        <h3 className='text-lg w-1/2 font-medium mb-2'>What's your name?</h3>
+                                    <div className='flex gap-4 mb-7'>
 
+                        <input 
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='First Name'
+                            value={firstName}
+                            onChange={(e) => {setFirstName(e.target.value)}}
+                        />
+                        <input 
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='Last Name'
+                            value={lastName}
+                            onChange={(e) => {setLastName(e.target.value)}}
+                        />
+                        </div>
+                        <h3 className='text-lg font-medium mb-2'>What's your email?</h3>
+                        <input 
+                            required
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}
+                            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+                            type="email"
+                            placeholder='email@example.com'
+                        />
+
+                        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+                        <input
+                            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                            required
+                            type="password"
+                            placeholder='password'
+                        />
+
+                        <button
+                            className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+                        >Create Account</button>
+
+                    </form>
+                    <p className='text-center'>Already have an account? <Link to='/login' className='text-blue-600'>Login here</Link></p>
+                </div>
             </div>
+              <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
+            Policy</span> and <span className='underline'>Terms of Service apply</span>.</p>
         </div>
+       
     )
 }
 
-export default Home;
+export default UserSignup;
