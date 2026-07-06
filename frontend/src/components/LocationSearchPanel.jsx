c8902054bbc6884b2755106ae487d1beaa06c974
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-07-06 21:08:37 +0530

    adding vehicles components

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 
 const LocationSearchPanel = () => {
+  //sample array for location search panel
 return(
-  <div>Location Search Panel</div>
+<div>
+   <div className='flex gap-4 border-2 p-3 active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex  items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3  active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3 active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3 active:border-black  items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3 active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3 active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+
+   <div className='flex gap-4 border-2 p-3  active:border-black items-center my-4 justify-start'>
+     <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i class="ri-map-pin-line"></i></h2>
+     <h4 className='font-medium'>77g,kamleshpuram near MMM university Gorakhpur</h4>
+  </div>
+</div>
+  
+
+  
+
+  
+  
+  
   )
 }
 export default LocationSearchPanel
