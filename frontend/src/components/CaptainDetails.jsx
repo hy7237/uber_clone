b61d8916d950d31689f4abc2569afbe233625338
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-07-11 19:18:27 +0530

    ridepopup

---
+++ frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const CaptainDetails=()=>
+{
+    return(
+        <div>
+               <div className='flex items-center justify-between'>
+                        <div className='flex items-center justify-start gap-3'>
+                            <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa00T62AWj3L0URW2KRoWJNL8Iv4etgmrcuT1r3ADVPg&s" alt="" />
+                            <h4 className='text-lg font-medium'>Himanshu Yadav</h4>
+                        </div>
+                        <div>
+                            <h4 className='text-xl font-semibold'>Rs 2312.20</h4>
+                            <p className='text-sm  text-gray-600'>Earned</p>
+                        </div>
+                       </div>
+                       <div className='flex p-3 mt-8 bg-gray-100 rounded-xl justify-center gap-5 items-start'>
+                        <div className='text-center'>
+                            <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+                            <h5 className='text-lg font-medium'>10.3</h5>
+                            <p className='text-sm text-gray-600'>Hours Online</p>
+                        </div>
+                        <div className='text-center'>
+                            <i className="text-3xl mb-2 font-thin ri-speed-up-fill"></i>
+                             <h5 className='text-lg font-medium'>10.3</h5>
+                            <p className='text-sm text-gray-600'>Hours Online</p>
+                        </div>
+                        <div className='text-center'>
+                            <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+                             <h5 className='text-lg font-medium'>10.3</h5>
+                            <p className='text-sm text-gray-600'>Hours Online</p>
+                        </div>
+                       </div>
+        </div>
+    )
+}
+
+export default CaptainDetails
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import {useState,useRef} from 'react'
 import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopup from '../components/RidePopup'
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
 
 const CaptainHome = () => {
+    const [ridePopupPanel,setRidePopupPanel]=useState(true);
+    const ridePopupPanelRef=useRef(null);
+      useGSAP(function() {
+        if(ridePopupPanel)
+        {
+           gsap.to(ridePopupPanelRef.current, {
+            transform:'translateY(0)'
+           })
+        }
+        else
+        {
+             gsap.to(ridePopupPanelRef.current, {
+            transform:'translateY(100%)'
+           })
+        }
+           
+    },[ridePopupPanel])
+
     return (
       <div className='h-screen'>
                  <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
@@ -10,37 +33,14 @@ const CaptainHome = () => {
                       <i className="text-lg font-medium ri-logout-box-r-line"></i>
                   </Link>
                  </div>
-                  <div className='h-1/2'>
+                  <div className='h-3/5'>
                    <img className='h-full w-full object-cover' src="https://acumengpstrack.com/wp-content/uploads/2020/10/41LXSSqXB5L.jpg" alt=""/>
                 </div>
-                <div className='h-1/2 p-4'>
-                       <div>
-                        <div>
-                            <img src="" alt="" />
-                            <h4>Himanshu Yadav</h4>
-                        </div>
-                        <div>
-                            <h4>Rs 2312.20</h4>
-                            <p>Earned</p>
-                        </div>
-                       </div>
-                       <div>
-                        <div className='text-center'>
-                            <i className="text-2xl font-thin ri-time-line"></i>
-                            <h5 className='text-lg font-medium'>10.3</h5>
-                            <p className='text-sm text-gray-600'>Hours Online</p>
-                        </div>
-                        <div className='text-center'>
-                            <i className="text-2xl font-thin ri-speed-up-fill"></i>
-                             <h5 className='text-lg font-medium'>10.3</h5>
-                            <p className='text-sm text-gray-600'>Hours Online</p>
-                        </div>
-                        <div className='text-center'>
-                            <i className="text-2xl font-thin ri-booklet-line"></i>
-                             <h5 className='text-lg font-medium'>10.3</h5>
-                            <p className='text-sm text-gray-600'>Hours Online</p>
-                        </div>
-                       </div>
+                <div className='h-2/5 p-4'>
+                       <CaptainDetails />
+                 </div>
+                <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                          <RidePopup setRidePopupPanel={setRidePopupPanel}/>
                  </div>
            </div>
     )
