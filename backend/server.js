79b5be692322d87b4e35edf500ce13c507d73fd6
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-07-28 18:08:45 +0530

    adding socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const http=require('http');
 const app=require('./app');
 const port=process.env.PORT||3000;
-
+const {initializeSocket}=require('./socket');//socket.io is used for persistent communication between client and server --here in this app Socket.io is used for the communication between user and captain
 const server=http.createServer(app);
+
+initializeSocket(server);
 server.listen(port,()=>
 {
     console.log(`Server is running on port ${port}`);
---
+++ backend/socket.js
@@ -0,0 +1,15 @@
+const socketIo=require('socket.io');
+const userModel=require('./models/user.model');
+const captainModel=require('.models/captain.model');
+
+
+
+function initializeSocket(server)
+{
+    //write the logic
+}
+
+const sendMessageToSocketId=(socketId,messageObject)=>
+{
+    //write the logic here 
+}
