b33b832ca446f028166560d98350c656fcf2ebfc
Himanshu Yadav <yadavhimanshu473@gmail.com>
2026-07-29 16:41:03 +0530

    socket implementation

--- backend/socket.js
+++ backend/socket.js
@@ -1,15 +1,70 @@
 const socketIo=require('socket.io');
 const userModel=require('./models/user.model');
-const captainModel=require('.models/captain.model');
+const captainModel=require('./models/captain.model');
+
+let io;
 
 
 
 function initializeSocket(server)
 {
-    //write the logic
+   io=socketIo(server,{
+    cors:
+    {
+        origin: '*',
+        methods:['GET','POST']
+    }
+   });
+   io.on('connetion',(socket)=>
+{
+    console.log(`Client connected:${socket.id}`);
+
+    socket.on('join',async(data)=>{
+        const {userId,userType}=data;
+
+        if(userType==='user')
+        {
+            await userModel.findByIdAndUpdate(userId,{socketId:socket.id});
+        }
+        else if(userType==='captain')
+        {
+            await userModel.findByIdAndUpdate(userId,{socketId:socket.id});
+        }
+    });
+    socket.on('update-location-captain',async(data)=>
+    {
+        const {userId,location}=data;
+
+        if(!location || !location.ltd || !location.lng)
+        {
+            return socket.emit('error',{message:'Invalid location data'});
+        }
+
+        await captainModel.findByIdAndUpdate(userId,{
+            location:
+            {
+                ltd:location.ltd,
+                lng:location.lng
+            }
+        });
+    });
+    socket.on('disconnect',()=>
+    {
+        console.log(`Clinet connected:${socket.id}`);
+    });
+});
 }
 
 const sendMessageToSocketId=(socketId,messageObject)=>
 {
-    //write the logic here 
+    console.log(messageObject);
+    if(io)
+    {
+        io.to(socketId).emit(messageObject.event,messageObject.data);
+    }
+    else{
+        console.log('Socket.io not initialized')
+    }
 }
+
+module.exports={initializeSocket,sendMessageToSocketId};
